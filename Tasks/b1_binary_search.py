3ad44da747b6c50a82cb8872798b9b8dbba3e4de
gi <ibragi_guzel@mail.ru>
2021-01-08 23:05:43 +0300

    реализован бинарный поиск , пройден test_missing

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,20 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
+    first_el = 0
+    last_el = len(arr) - 1
+    while first_el < last_el:
+        mid = (first_el + last_el)
+        elem_found = arr[mid]
+        if elem_found == elem:
+            return mid
+        if elem_found > elem:
+            last_el = mid - 1
+        else:
+            last_el = mid + 1
+        return None
+
+
+
     print(elem, arr)
     return None
