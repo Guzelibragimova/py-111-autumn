9f85bd5f13ce18a8508ced8af9b6b88eeddcd9f1
gi <ibragi_guzel@mail.ru>
2021-01-09 14:35:46 +0300

    реализованы функции нахождения чисел Фибоначчи итер-м и рек-ми способами

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,3 +1,4 @@
+
 def fib_recursive(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using recursive algorithm
@@ -5,9 +6,11 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
-
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 def fib_iterative(n: int) -> int:
     """
@@ -16,5 +19,11 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    a, b = 0, 1
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
