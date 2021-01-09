03a4f2ce1f5314c6f576e4a7dc5fb7a8968f289a
gi <ibragi_guzel@mail.ru>
2021-01-09 15:14:03 +0300

    реализована функция факториала рекурсивным способом

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,8 +4,9 @@ def factorial_recursive(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n == 0:
+        return 1
+    return factorial_recursive(n-1)*n
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +16,6 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
+
     print(n)
     return 0
