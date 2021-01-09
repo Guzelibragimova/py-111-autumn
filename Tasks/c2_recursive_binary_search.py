0b3072edf9e853ae6405db014d723bb44d259cce
gi <ibragi_guzel@mail.ru>
2021-01-09 21:02:13 +0300

    реализована рекурсивная реализация бинарного поиска, пройдены тесты test missing, test borders

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -9,5 +9,36 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+
+    first_el = 0
+    last_el = len(arr) - 1
+    mid = (first_el + last_el)//2
+    if first_el >=  last_el:
+        return None
+
+    if arr[last_el] == elem:
+        return last_el
+    if arr[first_el] == elem:
+        return first_el
+
+    while first_el < last_el:
+        elem_found = arr[mid]
+
+        if elem_found != elem:
+            return None
+        if elem_found == elem:
+            return mid
+        if elem_found > elem:
+            return binary_search(mid - 1, elem_found)
+        else:
+            return binary_search(mid + 1, elem_found)
+
+if __name__ == '__main__':
+    print(binary_search(300, arr=[7, 4, 3]))
+
+
+
+
+
+
+
