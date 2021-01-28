9379882796c4497f5afc483fbd5316516dffa26c
gi <ibragi_guzel@mail.ru>
2021-01-28 17:22:16 +0300

    реализована сортировка слиянием

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,35 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    from typing import Sequence, TypeVar
+    n = len(container)
+    if n < 2:
+        return container
+
+    l = sort(container[:n//2])
+    r = sort(container[n//2:n])
+
+    i = j = 0
+    res = []
+    while i < len(l) or j < len(r):
+        if not i < len(l):
+            res.append(r[j])
+            j += 1
+        elif not j < len(r):
+            res.append(l[i])
+            i += 1
+        elif l[i] < r[j]:
+            res.append(l[i])
+            i += 1
+        else:
+            res.append(r[j])
+            j += 1
+
+    return res
+
+
+
+
+
+
+
