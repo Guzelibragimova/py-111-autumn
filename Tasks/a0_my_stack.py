c06a1c630e0d103c3a0264ad6544c1215930c83b
gi <ibragi_guzel@mail.ru>
2021-01-29 23:50:23 +0300

    реализован стек

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -1,9 +1,6 @@
-"""
-My little Stack
-"""
 from typing import Any
 
-my_list_stack = [5, 6, 7] #вершина справа
+my_list_stack = [5, 6, 7]
 
 
 def push(elem: Any) -> None:
@@ -13,10 +10,6 @@ def push(elem: Any) -> None:
     :param elem: element to be pushed
     :return: Nothing
     """
-    # Операция добавления элемента на стек называется «push», удаления — «pop». Последний добавленный элемент называется верхушкой
-    # стека, или «top», и его можно посмотреть с помощью операции «peek»
-    # запушить значит на верх добавить, справа как append
-
 
     my_list_stack.append(elem)
     return None
@@ -30,9 +23,8 @@ def pop() -> Any:
     """
     if not my_list_stack:
         return None
-#pop принимает индекс и можно удалить любой элемент не только последний
-    return my_list_stack.pop()
 
+    return my_list_stack.pop()
 
 
 def peek(ind: int = 0) -> Any:
@@ -42,9 +34,6 @@ def peek(ind: int = 0) -> Any:
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
-    #insert добавляет на место индекса(с 0,1,2) в списке
-    # index позволяет узнать индекс или позицию элемента в последовательности
-    #a = my_list_stack.index(ind)
 
     print(ind)
     if ind >= len(my_list_stack):
@@ -60,26 +49,3 @@ def clear() -> None:
     """
     my_list_stack.clear()
     return None
-if __name__ == '__main__':
-    print(my_list_stack)
-    push(52)
-    # print(my_list_stack)
-
-    # pop()
-    # pop()
-    push(47)
-    # peek(6)
-    # clear()
-    # print(my_list_stack)
-    # push(4)
-    # print(my_list_stack)
-    #
-    # print(pop())
-    # print(my_list_stack)
-    # print(pop())
-
-    print(peek(0))
-    print(peek(1))
-    print(peek(2))
-
-
