06c1bd21c357c1b29253dc473d9ffc6a02a7db29
gi <ibragi_guzel@mail.ru>
2021-01-30 13:14:31 +0300

    реализован стек

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,31 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+
+    # print(g, start_node)
+    # return list(g.nodes)
+    visited = set()
+    if start_node not in visited:
+        print(start_node)
+        visited.add(start_node)
+        for neibh in (g[start_node] - visited):
+            dfs(g, neibh)
+        return visited
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+
+    print(graph.nodes)
+    print(graph['A'])
+    print(list(graph.neighbors('A')))
+
+
